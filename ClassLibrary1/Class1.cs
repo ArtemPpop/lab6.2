7adf0dd0e1e91bc1a3671300dde3a02c9aef7166
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-12-10 23:19:02 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int[] CalculatMinElement(int[,] matrx)
+        {
+            int lines = matrx.GetLength(0);
+            int cols = matrx.GetLength(1);
+            int[] minElem = new int[cols];
+
+            for (int j = 0; j < cols; j++)
+            {
+                int min = matrx[0, j];
+                for (int i = 1; i < lines; i++)
+                {
+                    if (matrx[i, j] < min)
+                    {
+                        min = matrx[i, j];
+                    }
+                }
+                minElem[j] = min;
+            }
+
+            return minElem;
+        }
+    }
+}
+
+
+
+
---
+++ lab6.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using ClassLibrary1;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        Console.Write("Введите количество строк: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int m = int.Parse(Console.ReadLine());
+
+        int[,] array = new int[n, m];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                array[i, j] = random.Next(1, 10);
+            }
+        }
+
+        Console.WriteLine("генерированный массив:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        int[] minElem = Class1.CalculatMinElement(array);
+
+        Console.WriteLine("Минимальный элемент из каждого столбца:");
+        foreach (var element in minElem)
+        {
+            Console.Write(element + " ");
+        }
+    }
+}
